1a7f2b63bd0c89e4c228ded617909783a22ead25
anonfedora <musaeleazar090@gmail.com>
2024-04-12 08:34:48 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb'), 
+    ProductModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

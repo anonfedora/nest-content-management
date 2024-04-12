75d2b794cf492def3c66c79663f6121000ad6333
anonfedora <musaeleazar090@gmail.com>
2024-04-12 09:02:18 +0100

    modules setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BlogModule } from './blog/blog.module';
+import { CategoryModule } from './category/category.module';
+import { CommentModule } from './comment/comment.module';
+import "dotenv/config";
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb'), 
-    ProductModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UserModule, AuthModule, BlogModule, CategoryModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductService {}
+export class UserService {}

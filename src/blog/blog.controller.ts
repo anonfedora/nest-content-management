42c49bb64a6058cb4afd721a3dfdf42368a5b32b
anonfedora <musaeleazar090@gmail.com>
2024-04-13 21:42:51 +0100

    blog: service

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -70,4 +70,65 @@ export class BlogController {
     async getAll(@Query("page") page: number, @Query("limit") limit: number) {
         return this.blogService.findAll(page, limit);
     }
+
+    @Public()
+    @Get("featured")
+    async getFeaturedPosts(): Promise<Blog[]> {
+        return this.blogService.findFeaturedPosts();
+    }
+
+    @Public()
+    @Get(":id/:category/:slug")
+    async getPostByIdCategoryAndSlug(
+        @Param("id") id: string,
+        @Param("category") category: string,
+        @Param("slug") slug: string
+    ): Promise<Blog> {
+        const post = await this.blogService.findByIdCategoryAndSlug(
+            id,
+            category,
+            slug
+        );
+        if (!post) {
+            throw new NotFoundException("Post not found");
+        }
+        return post;
+    }
+
+    @Get(":id/similar")
+    async findSimilarBlogs(@Param("id") id: string): Promise<Blog[]> {
+        const blog = await this.blogService.getPostById(id);
+        if (!blog) {
+            throw new NotFoundException("Blog not found");
+        }
+        const similarBlogs = await this.blogService.findSimilarBlogs(blog);
+        return similarBlogs;
+    }
+
+    @Public()
+    @Get("/author/:authorId")
+    async getBlogsByAuthor(
+        @Param("authorId") authorId: string
+    ): Promise<Blog[]> {
+        const blogs = await this.blogService.getBlogsByAuthor(authorId);
+        return blogs;
+    }
+
+    @Public()
+    @Get("category/:categoryName")
+    async getBlogsByCategory(
+        @Param("categoryName") categoryName: string
+    ): Promise<Blog[]> {
+        return this.blogService.findByIdCategory(categoryName);
+    }
+
+    @Public()
+    @Get("tags")
+    async getAllTags() {
+        const tags = await this.blogService.getAllTags();
+        const formattedTags = tags.map(tag => ({
+            name: tag.name,
+            count: tag.count
+        }));
+    }
 }

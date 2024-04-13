039f73723627536fafdb782d63efc399b1de7a00
anonfedora <musaeleazar090@gmail.com>
2024-04-13 14:24:13 +0100

    comments module

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,73 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Request,
+    Param,
+    Delete,
+    UseGuards,
+    NotFoundException,
+    UnauthorizedException
+} from "@nestjs/common";
+import { Role } from "../auth/schemas/auth.schema";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { Public } from "../auth/decorators/public.decorator";
+import { Comment } from "./schemas/comment.schema";
+import { RolesGuard } from "../auth/roles.guard";
+import { BlogService } from "../blog/blog.service";
+import { CommentService } from "./comment.service";
 
-@Controller('comment')
-export class CommentController {}
+@Controller("comment")
+export class CommentController {
+    constructor(
+        private readonly commentService: CommentService,
+        private blogService: BlogService
+    ) {}
+
+    @Post(":postId")
+    @UseGuards(RolesGuard)
+    @Roles(Role.Admin, Role.Guest)
+    async createComment(
+        @Param("postId") postId: string,
+        @Request() req,
+        @Body("content") content: string
+    ): Promise<Comment> {
+        const author = req.user._id;
+        const post = await this.blogService.getPostById(postId);
+        if (!post) {
+            throw new NotFoundException("Post not found");
+        }
+        return this.commentService.createComment(postId, author, content);
+    }
+
+    @Public()
+    @Get(":postId")
+    async getPostComments(
+        @Param("postId") postId: string
+    ): Promise<Commennt[]> {
+        return this.commentService.getPostComments(postId);
+    }
+
+    @Delete(":id")
+    @UseGuards(RolesGuard)
+    @Roles(Role.Admin, Role.Guest)
+    async deleteComment(@Param("id") id: string, @Request req) {
+        const comment = await this.commentService.getCommentById(id);
+        if (!comment) {
+            throw new NotFoundException(`Comment post with ID ${id} not found`);
+        }
+        const user = req.user;
+
+        if (
+            comment.author.toString() !== user._id.toString() &&
+            user.role !== "admin"
+        ) {
+            throw new UnauthorizedException(
+                `You are not authorized to delete this post`
+            );
+        }
+        await this.commentService.deleteComment(id);
+        return { message: `Blog post with ID ${id} deleted successfully` };
+    }
+}
--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { CommentService } from './comment.service';
-import { CommentController } from './comment.controller';
+import { Module } from "@nestjs/common";
+import { CommentService } from "./comment.service";
+import { CommentController } from "./comment.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Comment, CommentSchema } from "./schemas/comment.schema";
+import { BlogModule } from "../blog/blog.module";
 
 @Module({
-  providers: [CommentService],
-  controllers: [CommentController]
+    imports: [
+        MongooseModule.forFeature([{ name: "Comment", schema: CommentSchema }]),
+        forwardRef(() => BlogModule)
+    ],
+    providers: [CommentService],
+    controllers: [CommentController],
+    exports: [CommentService]
 })
 export class CommentModule {}
